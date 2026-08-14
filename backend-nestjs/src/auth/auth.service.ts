ffe891507db7b775d41b64aa75d3cd851876e101
Adryan Galdino Soares <adry4nbr@gmail.com>
2026-08-14 19:16:42 -0300

    Feat: Adiciona sistema de cadastro completo

--- backend-nestjs/src/auth/auth.service.ts
+++ backend-nestjs/src/auth/auth.service.ts
@@ -1,4 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+  async register(dto: RegisterDto) {
+    if (dto.password !== dto.confirmPassword) {
+      throw new BadRequestException('As duas senhas precisam ser iguais.');
+    }
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('Este email já está cadastrado.');
+    }
+
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+
+    const usuario = await this.prisma.user.create({
+      data: { name: dto.name, email: dto.email, password: hashedPassword },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        twoFactorEnabled: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+
+    return usuario;
+  }
+}
