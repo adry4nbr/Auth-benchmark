77b9db45dee22f56ead8de5b600e949d0aeda067
Adryan Galdino Soares <adry4nbr@gmail.com>
2026-08-28 16:28:24 -0300

    Feat: Adiciona prefixo api/v1 e CORS para o angular

--- backend-nestjs/src/main.ts
+++ backend-nestjs/src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200',
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
