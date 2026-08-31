ed1497ed45dd561a618555947d962712440c32df
Adryan Galdino Soares <adry4nbr@gmail.com>
2026-08-31 19:23:55 -0300

    Feat: Adiciona configuração inicial do springboot e sua conexão com o BD

---
+++ backend-springboot/backend-springboot/src/test/java/com/adryan/authbenchmark/backend_springboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adryan.authbenchmark.backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
